54af0da8720111522ecdbdc215f6bea398da4b6b
Ivan Fadila Putra <i.fadilaputra@gmail.com>
2018-10-07 19:21:10 +0700

    setup admin template to blade

---
+++ application/views/admin/template.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('admin.partials.head')
+</head>
+
+  <body>
+    <div id="wrapper">
+      <!-- Maiin Navigation -->
+      @include('admin.partials.nav')
+      <div id="page-wrapper">
+          <div class="container-fluid">
+            <!-- Main Content -->
+            @yield('content')
+          </div>
+      </div>
+    </div>
+    @include('admin.partials.scripts')
+  </body>
+
+</html>
