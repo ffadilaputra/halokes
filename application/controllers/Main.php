9a5c5b307fdc2e13cf6430233f4d649d88ff596a
Ivan Fadila Putra <i.fadilaputra@gmail.com>
2018-10-14 13:50:31 +0700

    main :monkey:

---
+++ application/controllers/Main.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+  class Main extends MY_Controller {
+
+    public function index(){
+      $this->view('front.page.main');
+    }
+}
+?>
---
+++ application/views/front/partials/header.blade.php
@@ -0,0 +1,48 @@
+<header class="top-area">
+  <div class="header-area">
+    <!-- Start Navigation -->
+    <nav class="navbar navbar-default bootsnav navbar-fixed dark no-background">
+
+      <div class="container">
+
+        <!-- Start Header Navigation -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+            <i class="fa fa-bars"></i>
+          </button>
+          <a class="navbar-brand" href="index.html">browny</a>
+        </div>
+        <!--/.navbar-header-->
+        <!-- End Header Navigation -->
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse menu-ui-design" id="navbar-menu">
+          <ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
+            <li class=" smooth-menu active"></li>
+            <li class=" smooth-menu">
+              <a href="#beranda">Beranda</a>
+            </li>
+            <li class="smooth-menu">
+              <a href="#about">Profil</a>
+            </li>
+            <li class="smooth-menu">
+              <a href="#experience">Info</a>
+            </li>
+            <li class="smooth-menu">
+            <a href="sample.html">PPDB</a>
+            </li>
+          </ul>
+          <!--/.nav -->
+        </div>
+        <!-- /.navbar-collapse -->
+      </div>
+      <!--/.container-->
+    </nav>
+    <!--/nav-->
+    <!-- End Navigation -->
+  </div>
+  <!--/.header-area-->
+
+  <div class="clearfix"></div>
+
+</header>
