d7b1c17d3f39022d7bbc9e8303a28117a87270db
imam <nawawi932@gmail.com>
2018-10-23 06:09:08 +0700

    buat panel mauidhoh

--- application/views/front/page/main.blade.php
+++ application/views/front/page/main.blade.php
@@ -229,222 +229,80 @@
       <h2>Mauidhoh</h2>
     </div>
     <div class="container">
-      <div class="experience-content">
-        <div class="main-timeline">
-          <ul>
-            <li>
-              <div class="single-timeline-box fix">
-                <div class="row">
-                  <div class="col-md-5">
-                    <div class="experience-time text-right">
-                      <h2>2018 - Present</h2>
-                      <h3>creative director</h3>
-                    </div>
-                    <!--/.experience-time-->
-                  </div>
-                  <!--/.col-->
-                  <div class="col-md-offset-1 col-md-5">
-                    <div class="timeline">
-                      <div class="timeline-content">
-                        <h4 class="title">
-                          <span>
-                            <i class="fa fa-circle" aria-hidden="true"></i>
-                          </span>
-                          hoplony tech limited
-                        </h4>
-                        <h5>newyork, USA</h5>
-                        <p class="description">
-                          Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore eu fugiat nulla pari. Excepteur sint occana
-                          inna tecat cupidatat non proident.
-                        </p>
-                      </div>
-                      <!--/.timeline-content-->
-                    </div>
-                    <!--/.timeline-->
-                  </div>
-                  <!--/.col-->
-                </div>
-              </div>
-              <!--/.single-timeline-box-->
-            </li>
-
-            <li>
-              <div class="single-timeline-box fix">
-                <div class="row">
-                  <div class="col-md-offset-1 col-md-5 experience-time-responsive">
-                    <div class="experience-time">
-                      <h2>
-                        <span>
-                          <i class="fa fa-circle" aria-hidden="true"></i>
-                        </span>
-                        2016 - 2018
-                      </h2>
-                      <h3>associate design director</h3>
-                    </div>
-                    <!--/.experience-time-->
-                  </div>
-                  <!--/.col-->
-                  <div class="col-md-5">
-                    <div class="timeline">
-                      <div class="timeline-content text-right">
-                        <h4 class="title">
-                          hoplony tech limited
-                        </h4>
-                        <h5>newyork, USA</h5>
-                        <p class="description">
-                          Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore eu fugiat nulla pari. Excepteur sint occana
-                          inna tecat cupidatat non proident.
-                        </p>
-                      </div>
-                      <!--/.timeline-content-->
-                    </div>
-                    <!--/.timeline-->
-                  </div>
-                  <!--/.col-->
-                  <div class="col-md-offset-1 col-md-5 experience-time-main">
-                    <div class="experience-time">
-                      <h2>
-                        <span>
-                          <i class="fa fa-circle" aria-hidden="true"></i>
-                        </span>
-                        2016 - 2018
-                      </h2>
-                      <h3>associate design director</h3>
-                    </div>
-                    <!--/.experience-time-->
-                  </div>
-                  <!--/.col-->
-                </div>
-              </div>
-              <!--/.single-timeline-box-->
-            </li>
-
-            <li>
-              <div class="single-timeline-box fix">
-                <div class="row">
-                  <div class="col-md-5">
-                    <div class="experience-time text-right">
-                      <h2>2013 - 2016</h2>
-                      <h3>senior UI/UX designer</h3>
-                    </div>
-                    <!--/.experience-time-->
-                  </div>
-                  <!--/.col-->
-                  <div class="col-md-offset-1 col-md-5">
-                    <div class="timeline">
-                      <div class="timeline-content">
-                        <h4 class="title">
-                          <span>
-                            <i class="fa fa-circle" aria-hidden="true"></i>
-                          </span>
-                          hoplony tech limited
-                        </h4>
-                        <h5>newyork, USA</h5>
-                        <p class="description">
-                          Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore eu fugiat nulla pari. Excepteur sint occana
-                          inna tecat cupidatat non proident.
-                        </p>
-                      </div>
-                      <!--/.timeline-content-->
-                    </div>
-                    <!--/.timeline-->
-                  </div>
-                  <!--/.col-->
-                </div>
-              </div>
-              <!--/.single-timeline-box-->
-            </li>
-
-            <li>
-              <div class="single-timeline-box fix">
-                <div class="row">
-                  <div class="col-md-offset-1 col-md-5 experience-time-responsive">
-                    <div class="experience-time">
-                      <h2>
-                        <span>
-                          <i class="fa fa-circle" aria-hidden="true"></i>
-                        </span>
-                        2012 - 2013
-                      </h2>
-                      <h3>UI/UX designer</h3>
-                    </div>
-                    <!--/.experience-time-->
-                  </div>
-                  <!--/.col-->
-                  <div class="col-md-5">
-                    <div class="timeline">
-                      <div class="timeline-content text-right">
-                        <h4 class="title">
-                          hoplony tech limited
-                        </h4>
-                        <h5>newyork, USA</h5>
-                        <p class="description">
-                          Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore eu fugiat nulla pari. Excepteur sint occana
-                          inna tecat cupidatat non proident.
-                        </p>
-                      </div>
-                      <!--/.timeline-content-->
-                    </div>
-                    <!--/.timeline-->
-                  </div>
-                  <!--/.col-->
-                  <div class="col-md-offset-1 col-md-5 experience-time-main">
-                    <div class="experience-time">
-                      <h2>
-                        <span>
-                          <i class="fa fa-circle" aria-hidden="true"></i>
-                        </span>
-                        2012 - 2013
-                      </h2>
-                      <h3>UI/UX designer</h3>
-                    </div>
-                    <!--/.experience-time-->
-                  </div>
-                  <!--/.col-->
-                </div>
-              </div>
-              <!--/.single-timeline-box-->
-            </li>
-
-            <li>
-              <div class="single-timeline-box fix">
-                <div class="row">
-                  <div class="col-md-5">
-                    <div class="experience-time text-right">
-                      <h2>2010 - 2012</h2>
-                      <h3>frontend developer</h3>
-                    </div>
-                    <!--/.experience-time-->
-                  </div>
-                  <!--/.col-->
-                  <div class="col-md-offset-1 col-md-5">
-                    <div class="timeline">
-                      <div class="timeline-content">
-                        <h4 class="title">
-                          <span>
-                            <i class="fa fa-circle" aria-hidden="true"></i>
-                          </span>
-                          hoplony tech limited
-                        </h4>
-                        <h5>newyork, USA</h5>
-                        <p class="description">
-                          Duis aute irure dolor in reprehenderit in vol patate velit esse cillum dolore eu fugiat nulla pari. Excepteur sint occana
-                          inna tecat cupidatat non proident.
-                        </p>
-                      </div>
-                      <!--/.timeline-content-->
-                    </div>
-                    <!--/.timeline-->
-                  </div>
-                  <!--/.col-->
-                </div>
-              </div>
-              <!--/.single-timeline-box-->
-            </li>
-
-          </ul>
+      <div class="experience-content col-lg-12">
+                
+        <div class="mau-panel col-lg-4 col-sm-12">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Panel title</h3>
+            </div>
+            <div class="panel-body">
+              Basic panel example
+            </div>
+          </div>
         </div>
-        <!--.main-timeline-->
+        <!-- end panel -->
+
+        <div class="mau-panel col-lg-4 col-sm-12">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Panel title</h3>
+            </div>
+            <div class="panel-body">
+              Basic panel example
+            </div>
+          </div>
+        </div>
+        
+        <!-- end panel -->
+        <div class="mau-panel col-lg-4 col-sm-12">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Panel title</h3>
+            </div>
+            <div class="panel-body">
+              Basic panel example
+            </div>
+          </div>
+        </div>
+        <!-- end panel -->
+
+        <div class="mau-panel col-lg-4 col-sm-12">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Panel title</h3>
+            </div>
+            <div class="panel-body">
+              Basic panel example
+            </div>
+          </div>
+        </div>
+        <!-- end panel -->
+
+        <div class="mau-panel col-lg-4 col-sm-12">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Panel title</h3>
+            </div>
+            <div class="panel-body">
+              Basic panel example
+            </div>
+          </div>
+        </div>
+        
+        <!-- end panel -->
+        <div class="mau-panel col-lg-4 col-sm-12">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Panel title</h3>
+            </div>
+            <div class="panel-body">
+              Basic panel example
+            </div>
+          </div>
+        </div>
+        <!-- end panel -->
+
       </div>
       <!--.mauidhoh-content-->
     </div>
