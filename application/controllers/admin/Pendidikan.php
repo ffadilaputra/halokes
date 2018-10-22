b2070e8d7fc67efbc1f5e3e0c002fcd53b077484
Ivan Fadila Putra <i.fadilaputra@gmail.com>
2018-10-22 18:58:38 +0700

    Gubluy

--- application/controllers/admin/Pendidikan.php
+++ application/controllers/admin/Pendidikan.php
@@ -4,8 +4,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
   class Pendidikan extends MY_Controller {
 
-    public function index(){
+    public function __construct(){
+      parent::__construct();
+      $this->load->model('PendidikanModel');
+    }
 
+    public function index(){
+      $data['list'] = PendidikanModel::all();
+    }
+
+    public function create(){
+      $this->view('');
+    }
+
+    public function store(){
+      $this->validate($this->input->post(),[
+
+      ]);
+      PendidikanModel::create($this->input->post());
+      var_dump($_POST);
+    }
+
+    public function edit($id){
+      $data['edit'] = PendidikanModel::find($id);
+
+    }
+
+    public function update($id){
+      $this->validate($this->input->post(),[
+
+      ]);
+      PendidikanModel::find($id)->update($this->input->post());
+    }
+
+    public function delete($id){
+      PendidikanModel::destroy($id);
     }
 
 }
