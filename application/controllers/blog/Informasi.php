ee9d970d2e01a264b0cf4b51193212d7a4c0a426
imam <nawawi932@gmail.com>
2018-12-02 21:39:53 +0700

    blog

--- application/controllers/blog/Informasi.php
+++ application/controllers/blog/Informasi.php
@@ -6,7 +6,7 @@ class Informasi extends MY_Controller {
     public function index()
     {
       $data['info'] = BeritaModel::orderBy('created_at', 'desc')->get();
-        $this->view('front.page.info.informasi', $data);
+      $this->view('front.page.info.informasi', $data);
     }
 
 }
