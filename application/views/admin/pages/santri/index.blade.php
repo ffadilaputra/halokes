645b9d2390f2f4a414bcb234320068e540698fe1
imam <nawawi932@gmail.com>
2018-12-26 11:22:27 +0700

    custom button ppdb verifikasi

--- application/views/admin/pages/santri/index.blade.php
+++ application/views/admin/pages/santri/index.blade.php
@@ -42,8 +42,8 @@
                     <td>{{ $data->nik }}</td>
                     <td>{{ date("d/m/Y",strtotime($data->tgl_lahir)) }}</td>
                     <td>
-                      <a class="btn btn-default" href="{{ base_url('admin/santri/show/').$data->id_santri }}"><i class="fa fa-eye"></i></a>
-                      <a class="btn btn-warning" href="{{ base_url('admin/verify/show/').$data->id_santri }}"><i class="fa fa-eye"></i></a>
+                      <a class="btn btn-info" href="{{ base_url('admin/santri/show/').$data->id_santri }}"><i class="fa fa-eye"></i>&nbsp;Detail</a>
+                      <a class="btn btn-success" href="{{ base_url('admin/verify/show/').$data->id_santri }}"><i class="fa fa-check-square-o" aria-hidden="true"></i> Verify</a>
                     </td>
                   </tr>
                   @endforeach
