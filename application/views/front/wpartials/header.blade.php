11986af33e0575d6f7c5c1340b8b9ea5863a806f
aurakanzaa <aurakanzaa@yahoo.co.id>
2019-01-03 22:36:43 +0700

    about done

--- application/views/front/wpartials/header.blade.php
+++ application/views/front/wpartials/header.blade.php
@@ -65,10 +65,10 @@
 
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="about.html">About</a>
+              <a class="nav-link" href="{{ base_url('about') }}">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="contact.html">Contact</a>
+              <a class="nav-link" href="{{ base_url('contact') }}">Contact</a>
             </li>
           </ul>
           
