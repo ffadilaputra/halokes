08a6a02543a5869c1e2906296fc5796ead15725b
aurakanzaa <aurakanzaa@yahoo.co.id>
2019-01-03 18:58:29 +0700

    captcha

--- application/views/front/page/ppdb_register.blade.php
+++ application/views/front/page/ppdb_register.blade.php
@@ -1,15 +1,23 @@
 @extends('front.template')
 @section('content')
 <!--contact start -->
-<?= form_open('ppdb/santri/store') ?>
+ <!-- form_open('ppdb/santri/store')  -->
+
+<?= form_open('ppdb/santri/store')
+// $atribut = array('id' => 'contact-form');
+// echo form_open('ppdb/santri/store', $atribut); 
+?>
+
 <section id="contact" class="contact">
   <br><br><br><br>
     <div class="section-heading text-center">
       <h2>FORMULIR PENDAFTARAN SANTRI BARU</h2>
     </div>
+
     <div class="container">
       <div class="contact-content">
         <div class="row">
+          <!-- right -->
           <div class="col-md-offset-1 col-md-5 col-sm-6">
             <div class="single-contact-box">
               <div class="contact-form">
@@ -59,6 +67,7 @@
                     <!--/.col-->
                   </div>
                   <!--/.row-->
+
                   <!-- nik -->
                   <div class="row">
                     <div class="col-sm-12">
@@ -107,13 +116,13 @@
                     <div class="col-sm-12">
                       <div class="form-group">
                         <label for="">Telepon</label>
-                        <div class="form-group input-group">
-                              <span class="input-group-addon" value="0">+62</span>
-                              <input type="number" name="telepon" class="form-control" maxlength="10">
+                        <!-- <div class="form-group input-group"> -->
+                              <!-- <span class="input-group-addon" value="0">+62</span> -->
+                              <input type="number" name="telepon" class="form-control" maxlength="13">
                               @if($errors->has('telepon'))
                               <small class="text-danger">{{ $errors->first('telepon') }}</small>
                               @endif
-                        </div>
+                        <!-- </div> -->
                       </div>
                       <!--/.form-group-->
                     </div>
@@ -127,21 +136,25 @@
             <!--/.single-contact-box-->
           </div>
           <!--/.col-->
+
+          <!-- left -->
           <div class="col-md-offset-1 col-md-5 col-sm-6">
             <div class="single-contact-box">
               <div class="contact-form">
+
                   <!-- alamat -->
                   <div class="row">
                     <div class="col-sm-12">
                       <label for="">Alamat</label>
                       <div class="form-group">
-                        <textarea name="alamat" class="form-control" rows="3" id="comment" placeholder="Message"></textarea>
+                        <textarea name="alamat" class="form-control" rows="3" id="comment"></textarea>
                       </div>
                       <!--/.form-group-->
                     </div>
                     <!--/.col-->
                   </div>
                   <!--/.row-->
+
                   <!-- kecamatan dan kelurahan -->
                   <div class="row">
                     <div class="col-sm-6 col-xs-12">
@@ -168,6 +181,7 @@
                     <!--/.col-->
                   </div>
                   <!--/.row-->
+
                   <!-- kota dan provinsi -->
                   <div class="row">
                     <div class="col-sm-6 col-xs-12">
@@ -194,7 +208,8 @@
                     <!--/.col-->
                   </div>
                   <!--/.row-->
-                  <!-- telp -->
+
+                  <!-- kodepos -->
                   <div class="row">
                     <div class="col-sm-12">
                       <div class="form-group">
@@ -209,6 +224,110 @@
                     <!--/.col-->
                   </div>
                   <!--/.row-->
+
+                  <!-- captcha -->
+                  <div class="form-group">
+                    <div class="g-recaptcha" data-sitekey="6LdesYUUAAAAAN9ObZshxalAifbSjPxoQK3vv0x4" data-callback="verifyRecaptchaCallback" data-expired-callback="expiredRecaptchaCallback"></div>
+                    <input class="form-control d-none" data-recaptcha="true" required data-error="Please complete the Captcha">
+                    <div class="help-block with-errors"></div>
+                  </div>
+                  <!-- end captcha -->
+
+                  <!-- captcha php -->
+                  <?php
+                  // require ReCaptcha class
+                  require('recaptcha/recaptcha-master/src/autoload.php');
+
+                  // configure
+                  // an email address that will be in the From field of the email.
+                  $from = 'Demo contact form <demo@domain.com>';
+
+                  // an email address that will receive the email with the output of the form
+                  $sendTo = 'Demo contact form <demo@domain.com>';
+
+                  // subject of the email
+                  $subject = 'New message from contact form';
+
+                  // form field names and their translations.
+                  // array variable name => Text to appear in the email
+                  $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
+
+                  // message that will be displayed when everything is OK :)
+                  $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+
+                  // If something goes wrong, we will display this message.
+                  $errorMessage = 'There was an error while submitting the form. Please try again later';
+
+                  // ReCaptch Secret
+                  $recaptchaSecret = '6LdesYUUAAAAAKcZbummOqyW7zO4U-u4NWmK0voJ';
+
+                  // let's do the sending
+
+                  // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+                  error_reporting(E_ALL & ~E_NOTICE);
+
+                  try {
+                      if (!empty($_POST)) {
+
+                          // validate the ReCaptcha, if something is wrong, we throw an Exception,
+                          // i.e. code stops executing and goes to catch() block
+                          
+                          if (!isset($_POST['g-recaptcha-response'])) {
+                              throw new \Exception('ReCaptcha is not set.');
+                          }
+
+                          // do not forget to enter your secret key from https://www.google.com/recaptcha/admin
+                          
+                          $recaptcha = new \ReCaptcha\ReCaptcha($recaptchaSecret, new \ReCaptcha\RequestMethod\CurlPost());
+                          
+                          // we validate the ReCaptcha field together with the user's IP address
+                          
+                          $response = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+
+                          if (!$response->isSuccess()) {
+                              throw new \Exception('ReCaptcha was not validated.');
+                          }
+                          
+                          // everything went well, we can compose the message, as usually
+                          
+                          $emailText = "You have a new message from your contact form\n=============================\n";
+
+                          foreach ($_POST as $key => $value) {
+                              // If the field exists in the $fields array, include it in the email
+                              if (isset($fields[$key])) {
+                                  $emailText .= "$fields[$key]: $value\n";
+                              }
+                          }
+                      
+                          // All the neccessary headers for the email.
+                          $headers = array('Content-Type: text/plain; charset="UTF-8";',
+                              'From: ' . $from,
+                              'Reply-To: ' . $from,
+                              'Return-Path: ' . $from,
+                          );
+                          
+                          // Send email
+                          mail($sendTo, $subject, $emailText, implode("\n", $headers));
+
+                          $responseArray = array('type' => 'success', 'message' => $okMessage);
+                      }
+                  } catch (\Exception $e) {
+                      $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
+                  }
+
+                  if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+                      $encoded = json_encode($responseArray);
+
+                      header('Content-Type: application/json');
+
+                      echo $encoded;
+                  } else {
+                      echo $responseArray['message'];
+                  }
+                  ?>
+                  <!-- end captcha php -->
+
+                  <!-- btn submit -->
                   <div class="row">
                     <div class="col-sm-12">
                         <button type="submit" class="btn btn-success">Submit</button>
@@ -229,8 +348,12 @@
       <!--/.contact-content-->
     </div>
     <!--/.container-->
-  </section>
-  <?= form_close() ?>
+</section>
+<?= form_close() ?>
   <!--/.contact-->
   <!--contact end -->
+
+  <script>
+  
+  </script>
 @endsection
--- application/views/front/partials/scripts.blade.php
+++ application/views/front/partials/scripts.blade.php
@@ -26,3 +26,45 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
   <!--Custom JS-->
   <script src="{{ base_url('assets/browny') }}/assets/js/custom.js"></script>
+
+  <!-- captcha -->
+  <script src='https://www.google.com/recaptcha/api.js'></script>
+
+  <script>
+  $(function () {
+
+  window.verifyRecaptchaCallback = function (response) {
+      $('input[data-recaptcha]').val(response).trigger('change')
+  }
+
+  window.expiredRecaptchaCallback = function () {
+      $('input[data-recaptcha]').val("").trigger('change')
+  }
+
+  $('#contact-form').validator();
+
+  $('#contact-form').on('submit', function (e) {
+      if (!e.isDefaultPrevented()) {
+          var url = "contact.php";
+
+          $.ajax({
+              type: "POST",
+              url: url,
+              data: $(this).serialize(),
+              success: function (data) {
+                  var messageAlert = 'alert-' + data.type;
+                  var messageText = data.message;
+
+                  var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                  if (messageAlert && messageText) {
+                      $('#contact-form').find('.messages').html(alertBox);
+                      $('#contact-form')[0].reset();
+                      grecaptcha.reset();
+                  }
+              }
+          });
+          return false;
+      }
+  })
+  });
+  </script>
