dac545e5b873b28aee09b82dd599a17ead3251fa
aurakanzaa <aurakanzaa@yahoo.co.id>
2019-01-28 15:19:09 +0700

    pdf

--- application/views/front/page/cetak_kartu.blade.php
+++ application/views/front/page/cetak_kartu.blade.php
@@ -10,13 +10,13 @@
 		margin: 0 auto;
 	}
 	table th{
-		border:0px solid #000;
+		border:1px solid #000;
 		padding: 3px;
 		font-weight: bold;
 		text-align: center;
 	}
 	table td{
-		border: 0px solid #000;
+		border: 1px solid #000;
 		padding: 3px;
 		vertical-align: top;
 	}
@@ -30,14 +30,14 @@
         <div class="card">
             <!-- tabel header -->
             <table>
-                <tr>
+                <tr align="center">
                     
                     <td colspan="" align="right" style="border: 0px"><br><br>
                         <img src="{{ base_url('assets/wordify/') }}images/logo.jpg" alt="" align="center" width="128" height="120"> 
                     </td>
                     <td colspan="" style="text-align: left; border: 0px">
                         <br><br><br>
-                        <h5>
+                        <h5 style="padding-left:40px;">
                         YAYASAN MA'HAD DARUN NAJAH<br>
                         PONDOK PESANTREN PPAI DARUN NAJAH<br>
                         Jl. Pesantren, No. 51, Ngijo, Karangploso, Malang, (0341)5034501<br>
@@ -54,22 +54,26 @@
                     <!-- tabel isi --> 
                     <table>
                         <tr>
-                            <img src="" alt="" align="right" style="width:170px; height:220px; background-color:#eeeeee"> 
+                            
                         </tr>
                         <tr>
+                            <th><img src="" alt="" align="left" style="width:150px; height:200px; background-color:#eeeeee"> </th>
                             <th style="text-align:right;padding-right:10px;">Nama</th>
                             <td style="padding-left:20px;">Aura Kanza Caesaria</td>
                         </tr>
                         
                         <tr>
+                            <th></th>
                             <th style="text-align:right;padding-right:10px;">TTL</th>
                             <td style="padding-left:20px;">Banjarmasin, 11 Februari 1998</td>
                         </tr>
                         <tr>
+                            <th></th>
                             <th style="text-align:right;padding-right:10px;">Alamat</th>
                             <td style="padding-left:20px;">Jl. Soekarno Hatta no. 9</td>
                         </tr>
                         <tr>
+                            <th></th>
                             <th style="text-align:right;padding-right:10px;">No Telepon</th>
                             <td style="padding-left:20px;">0341-321433</td>
                         </tr>
@@ -105,33 +109,35 @@
             
                 <!-- tabel footer -->
                 <div class="row">
-                    <div class="col-md-6 col-lg-6">
-                        <table>
-                            <tr>
-                                <td>Pengumuman</td>
-                            </tr>
-                            <tr>
-                                <td>Pembayaran PPDB</td>
-                                <td>: 1-31 Mei 2019</td>
-                            </tr>
-                            <tr>
-                                <td>Checkin Pesantren</td>
-                                <td>: 21 Juli 2019</td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="col-md-6 col-lg-6">
-                        <table>
-                            <tr>
-                                <td style="text-align:right;">Malang,1 Januari 2019</td>
-                            </tr>
-                            <tr>
-                                <td><br></td>
-                            </tr>
-                            <tr>
-                                <td style="text-align:right;">Petugas</td>
-                            </tr>
-                        </table>
+                    <div class="col-md-12 col-lg-12">
+                        <div class="col-md-6 col-lg-6">
+                            <table>
+                                <tr>
+                                    <td>Pengumuman</td>
+                                </tr>
+                                <tr>
+                                    <td>Pembayaran PPDB</td>
+                                    <td>: 1-31 Mei 2019</td>
+                                </tr>
+                                <tr>
+                                    <td>Checkin Pesantren</td>
+                                    <td>: 21 Juli 2019</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-md-6 col-lg-6">
+                            <table>
+                                <tr>
+                                    <td style="text-align:right;">Malang,1 Januari 2019</td>
+                                </tr>
+                                <tr>
+                                    <td><br></td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align:right;">Petugas</td>
+                                </tr>
+                            </table>
+                        </div>
                     </div>
                 </div>
                 <!-- tabel footer end -->
