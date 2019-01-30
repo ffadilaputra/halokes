ece1841c69f7370bc7ffa4de54af2514594c6eb2
Ivan Fadila Putra <i.fadilaputra@gmail.com>
2019-01-30 09:06:43 +0700

    Fx scripts to async and remove ununsed file

--- application/views/front/wpartials/footer.blade.php
+++ application/views/front/wpartials/footer.blade.php
@@ -32,7 +32,7 @@
          <div class="col-md-12 text-center">
             <p class="small">
                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-               Copyright &copy; <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All Rights Reserved | Design by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+               Copyright &copy; <script>document.write(new Date().getFullYear());</script> All Rights Reserved | Design by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             </p>
          </div>
--- application/views/front/wpartials/scripts.blade.php
+++ application/views/front/wpartials/scripts.blade.php
@@ -1,13 +1,13 @@
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/esm/popper-utils.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
-<script src="{{ base_url('assets/wordify') }}/js/owl.carousel.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.0/jquery.waypoints.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/stellar.js/0.6.2/jquery.stellar.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.0.0/jquery-migrate.min.js"></script>
-<script src="{{ base_url('assets/wordify') }}/js/main.js"></script>
-<script src="{{ base_url('assets/js/instagram_api.js') }}"></script>
-<script src="{{ base_url('assets/js/register_validation.js') }}"></script>
-<script src='https://www.google.com/recaptcha/api.js'></script>
+<script async src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" defer></script>
+<script async src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/esm/popper-utils.min.js" defer></script>
+<script async src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js" defer></script>
+<script async src="{{ base_url('assets/wordify') }}/js/owl.carousel.min.js"></script>
+<script async src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.0/jquery.waypoints.min.js" defer></script>
+<script async src="https://cdnjs.cloudflare.com/ajax/libs/stellar.js/0.6.2/jquery.stellar.min.js" defer></script>
+<script async src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.0.0/jquery-migrate.min.js" defer></script>
+<script async src="{{ base_url('assets/wordify') }}/js/main.js" defer></script>
+<script async src="{{ base_url('assets/js/instagram_api.js') }}" defer></script>
+<script async src="{{ base_url('assets/js/register_validation.js') }}" defer></script>
+<script async src='https://www.google.com/recaptcha/api.js' defer></script>
 
